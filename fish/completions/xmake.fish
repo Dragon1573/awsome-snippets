ca3a95ac17bf7553836611998810f7dd683f58f3
Dragon1573 <49941141+Dragon1573@users.noreply.github.com>
2023-02-25 13:36:05 +0800

    XMake: 更新配置文件
    - 移除缩写子命令，只提供完整子命令的补全
    - 重命名自定义函数
    
    Signed-off-by: Dragon1573 <49941141+Dragon1573@users.noreply.github.com>

--- fish/completions/xmake.fish
+++ fish/completions/xmake.fish
@@ -1,12 +1,13 @@
-# 子命令组
-set __ACTIONS_SUBCOMMAND i install c clean u uninstall update service b build g global q require f config r run p package create
-set __PLUGINS_SUBCOMMAND doxygen repo plugin format project l lua check show watch m macro
+# Subcommand group
+set __ACTIONS_SUBCOMMAND install clean uninstall update service build global require config run package create
+set __PLUGINS_SUBCOMMAND doxygen repo plugin format project lua check show watch macro
 
 
-function __fish_xmake_complete_no_subcommand
-    # 扫描当前命令行里面有没有包含子命令
+function __xmake_complete_no_subcommand
+    # Check if current commandline contains the above subcommands
     for i in (commandline -opc)
-        # XMake 提供了如下的操作和插件命令，它们都是子命令，需要单独标记
+        # XMake provides following subcommands for "Action" and "Plugin"
+        # We have to seperate them
         if contains -- $i $__ACTIONS_SUBCOMMAND $__PLUGINS_SUBCOMMAND
             return 1
         end
@@ -31,29 +32,29 @@ complete -c xmake -s P -l project -r -d 'Change to the given project directory.'
 
 ### 子命令（Task）
 # 操作
-complete -c xmake -f -n __fish_xmake_complete_no_subcommand -a install -d 'Package and install the target binary files.'
-complete -c xmake -f -n __fish_xmake_complete_no_subcommand -a clean -d 'Remove all binary and temporary files.'
-complete -c xmake -f -n __fish_xmake_complete_no_subcommand -a uninstall -d 'Uninstall the project binary files.'
-complete -c xmake -f -n __fish_xmake_complete_no_subcommand -a update -d 'Update and uninstall the xmake program.'
-complete -c xmake -f -n __fish_xmake_complete_no_subcommand -a service -d 'Start service for remote or distributed compilation and etc.'
-complete -c xmake -f -n __fish_xmake_complete_no_subcommand -a build -d 'Build targets if no given tasks.'
-complete -c xmake -f -n __fish_xmake_complete_no_subcommand -a global -d 'Configure the global options for xmake.'
-complete -c xmake -f -n __fish_xmake_complete_no_subcommand -a require -d 'Install and update required packages.'
-complete -c xmake -f -n __fish_xmake_complete_no_subcommand -a config -d 'Configure the project.'
-complete -c xmake -f -n __fish_xmake_complete_no_subcommand -a run -d 'Run the project target.'
-complete -c xmake -f -n __fish_xmake_complete_no_subcommand -a package -d 'Package target.'
-complete -c xmake -f -n __fish_xmake_complete_no_subcommand -a create -d 'Create a new project.'
+complete -c xmake -f -n __xmake_complete_no_subcommand -a install -d 'Package and install the target binary files.'
+complete -c xmake -f -n __xmake_complete_no_subcommand -a clean -d 'Remove all binary and temporary files.'
+complete -c xmake -f -n __xmake_complete_no_subcommand -a uninstall -d 'Uninstall the project binary files.'
+complete -c xmake -f -n __xmake_complete_no_subcommand -a update -d 'Update and uninstall the xmake program.'
+complete -c xmake -f -n __xmake_complete_no_subcommand -a service -d 'Start service for remote or distributed compilation and etc.'
+complete -c xmake -f -n __xmake_complete_no_subcommand -a build -d 'Build targets if no given tasks.'
+complete -c xmake -f -n __xmake_complete_no_subcommand -a global -d 'Configure the global options for xmake.'
+complete -c xmake -f -n __xmake_complete_no_subcommand -a require -d 'Install and update required packages.'
+complete -c xmake -f -n __xmake_complete_no_subcommand -a config -d 'Configure the project.'
+complete -c xmake -f -n __xmake_complete_no_subcommand -a run -d 'Run the project target.'
+complete -c xmake -f -n __xmake_complete_no_subcommand -a package -d 'Package target.'
+complete -c xmake -f -n __xmake_complete_no_subcommand -a create -d 'Create a new project.'
 # 插件
-complete -c xmake -f -n __fish_xmake_complete_no_subcommand -a doxygen -d 'Generate the doxygen document.'
-complete -c xmake -f -n __fish_xmake_complete_no_subcommand -a repo -d 'Manage package repositories.'
-complete -c xmake -f -n __fish_xmake_complete_no_subcommand -a plugin -d 'Manage plugins of xmake.'
-complete -c xmake -f -n __fish_xmake_complete_no_subcommand -a format -d 'Format the current project.'
-complete -c xmake -f -n __fish_xmake_complete_no_subcommand -a project -d 'Generate the project file.'
-complete -c xmake -f -n __fish_xmake_complete_no_subcommand -a lua -d 'Run the lua script.'
-complete -c xmake -f -n __fish_xmake_complete_no_subcommand -a check -d 'Check the project sourcecode and configuration.'
-complete -c xmake -f -n __fish_xmake_complete_no_subcommand -a show -d 'Show the given project information.'
-complete -c xmake -f -n __fish_xmake_complete_no_subcommand -a watch -d 'Watch the project directories and run command.'
-complete -c xmake -f -n __fish_xmake_complete_no_subcommand -a macro -d 'Run the given macro.'
+complete -c xmake -f -n __xmake_complete_no_subcommand -a doxygen -d 'Generate the doxygen document.'
+complete -c xmake -f -n __xmake_complete_no_subcommand -a repo -d 'Manage package repositories.'
+complete -c xmake -f -n __xmake_complete_no_subcommand -a plugin -d 'Manage plugins of xmake.'
+complete -c xmake -f -n __xmake_complete_no_subcommand -a format -d 'Format the current project.'
+complete -c xmake -f -n __xmake_complete_no_subcommand -a project -d 'Generate the project file.'
+complete -c xmake -f -n __xmake_complete_no_subcommand -a lua -d 'Run the lua script.'
+complete -c xmake -f -n __xmake_complete_no_subcommand -a check -d 'Check the project sourcecode and configuration.'
+complete -c xmake -f -n __xmake_complete_no_subcommand -a show -d 'Show the given project information.'
+complete -c xmake -f -n __xmake_complete_no_subcommand -a watch -d 'Watch the project directories and run command.'
+complete -c xmake -f -n __xmake_complete_no_subcommand -a macro -d 'Run the given macro.'
 
 ### 子命令相关参数补全
 # build
