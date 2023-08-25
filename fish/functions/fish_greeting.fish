d28bc997091f83555d288c6cf2d2d1491b4c315b
Dragon1573 <49941141+Dragon1573@users.noreply.github.com>
2023-08-25 15:54:48 +0800

    更新欢迎词

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,6 +1,4 @@
 function fish_greeting
-    echo "欢迎使用 Ubuntu 22.10 Kinetic 和 Fish Shell ！"
+    echo "欢迎使用 $(lsb_release -d -s) 和 Fish Shell ！"
     echo -e '输入 \e[32mhelp\e(B\e[m 查看使用指南'
-    echo
-    fortune-zh
 end
